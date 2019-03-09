549b52d44025195a4488d5731262fca069d1d27a
Miodrag Popović <miodragpopovic@stcable.net>
2019-03-09 12:36:58 +0100

    changed version number to 0.8 beta

--- src/java/com/vaklinov/zerowallet/ZeroWallet.java
+++ src/java/com/vaklinov/zerowallet/ZeroWallet.java
@@ -100,7 +100,7 @@ public class ZeroWallet
     public ZeroWallet(StartupProgressDialog progressDialog)
         throws IOException, InterruptedException, WalletCallException
     {
-        super("ZeroClassic Swing Wallet - 0.74 (beta)");
+        super("ZeroClassic Swing Wallet - 0.8 (beta)");
         
         if (progressDialog != null)
         {
